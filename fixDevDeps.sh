7ba185643bb7c75e54f1aa9ce046b52b79f03415
Matt Brown <anothermattbrown@gmail.com>
2016-05-12 16:50:12 -0700

    oh yeah, and correct 1.0.0 before updating the rest

--- fixDevDeps.sh
+++ fixDevDeps.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# fix the dependency on "should" package
+json -I -f mocha-1.0.0/package.json -e 'this.devDependencies.should="<2"'
+
 devDeps=$(json -f mocha-1.0.0/package.json devDependencies)
 
 for d in mocha-*
